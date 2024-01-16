42d4b53e950832f0d37953a0201f6a94b07fb725
Vraj <vrajaparikh01@gmail.com>
2024-01-16 23:30:09 +0530

    feat: deploy script and hardhat config for mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ethers");
+// require("@nomiclabs/hardhat-etherscan");
+require("hardhat-abi-exporter");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const defaultKey =
+  "0000000000000000000000000000000000000000000000000000000000000000";
+
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const MUMBAI_URL = process.env.GOERLI_URL;
+const OPTIMIZER_RUNS = process.env.OPTIMIZER_RUNS;
+const OPTIMIZER_FLAG = process.env.OPTIMIZER_FLAG;
+
 module.exports = {
   solidity: {
     compilers: [
@@ -9,11 +21,48 @@ module.exports = {
       { version: "0.6.6" },
     ],
   },
+  settings: {
+    optimizer: {
+      enabled: OPTIMIZER_FLAG || true,
+      runs: parseInt(OPTIMIZER_RUNS) || 200,
+    },
+    evmVersion: "istanbul",
+  },
+  etherscan: {
+    apiKey: {
+      mumbai: ETHERSCAN_API_KEY,
+    },
+  },
   networks: {
+    mumbai: {
+      url:
+        MUMBAI_URL ||
+        "https://polygon-mumbai.g.alchemy.com/v2/ystrrbBKtsvf4x416CItuRU6mn7QlEB9",
+      accounts: [ACCOUNT_PRIVATE_KEY || defaultKey],
+    },
     hardhat: {
       forking: {
         url: "https://bsc-dataseed1.binance.org/",
       },
+      chainID: 31337,
     },
   },
+  abiExporter: [
+    {
+      path: "./abi/json",
+      runOnCompile: true,
+      clear: true,
+      flat: true,
+      spacing: 2,
+      format: "json",
+    },
+    {
+      path: "./abi/minimal",
+      runOnCompile: true,
+      clear: true,
+      flat: true,
+      spacing: 2,
+      format: "minimal",
+    },
+  ],
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,10 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
-async function main() {}
+async function main() {
+  const FlashLoan = await ethers.getContractFactory("FlashLoan");
+  let flashLoan = await FlashLoan.deploy();
+  console.log("Flash Loan contract deployed to: ", flashLoan.target);
+}
 
 main().catch((error) => {
   console.error(error);
